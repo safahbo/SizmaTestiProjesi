8f9d8cc265c7327a114f639c6bc2fd3504a0424e
safahbo <safahbo61@gmail.com>
2026-04-05 05:57:56 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-
+{
+  "name": "idor-zafiyet-analizi",
+  "version": "1.0.0",
+  "description": "Sızma Testi Dersi IDOR Projesi",
+  "main": "src/server.js",
+  "scripts": {
+    "start": "node src/server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
